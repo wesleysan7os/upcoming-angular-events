af327c83979e3df71dba2e7367c61ed2bd69e419
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-01-08 14:37:32 -0300

    loading static files

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  templateUrl: './events-app.component.html',
+})
+export class EventsAppComponent {
+  title = 'upcoming-angular-events';
+}
