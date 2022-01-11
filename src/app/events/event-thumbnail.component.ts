208befd70487f2165781b08bd9cdf7e61e5690fa
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-01-10 22:53:58 -0300

    displaying event list moc data

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -17,6 +17,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
     </div>
   `,
   styles: [`
+    .thumbnail { min-height: 210px; }
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
   `]
