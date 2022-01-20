9e001148fc4ccccec7cd2a17d6bf9154e115acd0
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-01-19 22:43:17 -0300

    organizing exports with Barrels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ToastrService } from './commom/toastr.service';
 
-import { EventsAppComponent } from './events-app.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventService } from './events/shared/event.service';
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventListResolver,
+  EventRouteActivator,
+} from './events/index';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { Error404Component } from './errors/404.component';
+import { EventsAppComponent } from './events-app.component';
+import { EventService } from './events/shared/event.service';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
-import { CreateEventComponent } from './events/create-event.component';
-import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-route-activator.service';
-import { EventListResolver } from './events/events-list-resolver.service';
 
 @NgModule({
   imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
