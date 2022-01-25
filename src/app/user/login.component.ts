6396065cf24bcc66131c088b4af0e5a6798bb75d
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-01-24 22:07:42 -0300

    validating login template-based form

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,13 +1,18 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Component({
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styles: [`
+    em { float: right; color: #E05C65; padding-left: 10px; }
+  `]
 })
 export class LoginComponent {
   userName: string;
   password: string;
+  mouseoverLogin: boolean;
 
   constructor(private authService: AuthService, private router: Router) { }
 
